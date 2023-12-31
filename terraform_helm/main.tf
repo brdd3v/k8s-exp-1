12a910119a60bcc2080d72220d78a0c964970bf7
brdd3v <brddev@yandex.ru>
2023-12-31 12:16:40 +0100

    Add missing k8s provider, specifying context

--- terraform_helm/main.tf
+++ terraform_helm/main.tf
@@ -1,10 +1,19 @@
 terraform {
   required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.24.0"
+    }
     helm = {
       source  = "hashicorp/helm"
       version = "2.12.1"
     }
   }
+}
+
+provider "kubernetes" {
+  config_path    = "~/.kube/config"
+  config_context = "minikube"
 }
 
 provider "helm" {
